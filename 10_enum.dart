25e1ad08366d5e4a7783cc53eefe93ab4c04c1ce
ishtiaqmahmood <ishtiaqmahmood45@gmail.com>
2026-06-04 11:02:34 +0600

    feat: updated enums

--- 10_enum.dart
+++ 10_enum.dart
@@ -12,6 +12,28 @@ void main() {
       print("Your favorite color is blue.");
       break;
   }
+
+  describe(Season.winter); // Very cold
 }
 
+// An enum (enumeration) is a special type that represents a fixed set of named constants. Use it when a variable can only be one of a limited set of values.
 enum Color { red, green, blue }
+
+enum Season { spring, summer, autumn, winter }
+
+void describe(Season s) {
+  switch (s) {
+    case Season.spring:
+      print('Flowers bloom');
+      break;
+    case Season.summer:
+      print('Very hot');
+      break;
+    case Season.autumn:
+      print('Leaves fall');
+      break;
+    case Season.winter:
+      print('Very cold');
+      break;
+  }
+}
